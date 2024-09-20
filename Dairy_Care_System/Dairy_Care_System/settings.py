d757b548ba79ef1b9062b2cb238e5e75769cc753
KRISHNENDULAL <krishnendulal2025@mca.ajce.in>
2024-09-20 20:28:44 +0530

    commit

--- Dairy_Care_System/Dairy_Care_System/settings.py
+++ Dairy_Care_System/Dairy_Care_System/settings.py
@@ -134,8 +134,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'krishnendulal113@gmail.com'
-EMAIL_HOST_PASSWORD = 'blmq erlr ogso uzxj'
+EMAIL_HOST_USER = 'dairycaresystem25@gmail.com'
+EMAIL_HOST_PASSWORD = '####'
 DEFAULT_FROM_EMAIL = 'admin@yourdomain.com'
 
 
