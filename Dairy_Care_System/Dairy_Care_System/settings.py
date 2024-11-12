86f3579773a1020a137ff034ae9a6c5821a2972f
KRISHNENDULAL <krishnendulal2025@mca.ajce.in>
2024-11-12 22:40:27 +0530

    Final Submission of Mini Project

--- Dairy_Care_System/Dairy_Care_System/settings.py
+++ Dairy_Care_System/Dairy_Care_System/settings.py
@@ -113,6 +113,8 @@ WSGI_APPLICATION = 'Dairy_Care_System.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
